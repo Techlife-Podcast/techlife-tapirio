a2813d122e447de45e4ab2af89a6bef1fd90aebf
Vasily S. Myazin <productizement@gmail.com>
2025-08-22 23:33:03 -0300

    feat: add helmet.js security headers with comprehensive CSP allowing all required domains for images and media

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const sassMiddleware = require("sass-middleware");
+const helmet = require("helmet");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -21,19 +22,44 @@ const app = express();
 app.set("views", path.join(__dirname, "pages"));
 app.set("view engine", "pug");
 
+// Security headers with helmet
+app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+            defaultSrc: ["'self'"],
+            styleSrc: ["'self'", "'unsafe-inline'", "fonts.googleapis.com", "stackpath.bootstrapcdn.com"],
+            fontSrc: ["'self'", "fonts.gstatic.com"],
+            scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", "www.googletagmanager.com", "www.google-analytics.com", "unpkg.com", "plausible.io"],
+            imgSrc: ["'self'", "data:", "*.tapirio.com", "techlifepodcast.com", "*.techlifepodcast.com", "www.google-analytics.com"],
+            mediaSrc: ["'self'", "*.tapirio.com", "techlifepodcast.com", "*.techlifepodcast.com", "*.amazonaws.com"],
+            connectSrc: ["'self'", "www.google-analytics.com", "plausible.io"],
+            frameSrc: ["'none'"],
+            objectSrc: ["'none'"],
+            baseUri: ["'self'"],
+            formAction: ["'self'"]
+        }
+    },
+    crossOriginEmbedderPolicy: false, // For podcast audio embedding
+    hsts: {
+        maxAge: 31536000,
+        includeSubDomains: true,
+        preload: true
+    }
+}));
+
 // Middleware setup
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
-  sassMiddleware({
-    src: path.join(__dirname, "scss"),
-    dest: path.join(__dirname, "public/stylesheets"),
-    debug: true,
-    outputStyle: "compressed",
-    prefix: "/stylesheets",
-  })
+    sassMiddleware({
+        src: path.join(__dirname, "scss"),
+        dest: path.join(__dirname, "public/stylesheets"),
+        debug: true,
+        outputStyle: "compressed",
+        prefix: "/stylesheets",
+    })
 );
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -46,23 +72,23 @@ const project = new Project(BLOG_PATH, { podcastFeedXml: PODCAST_FEED_XML });
 
 // This should be an async function
 async function initializeProject() {
-  await project.init();
-  project.sortBy({ property: "date", asc: false });
+    await project.init();
+    project.sortBy({ property: "date", asc: false });
 
-  const podcast = project.podcastModule.json.rss;
-  const episodes = podcast.channel.item.map((episode) => {
-    return episode;
-  });
+    const podcast = project.podcastModule.json.rss;
+    const episodes = podcast.channel.item.map((episode) => {
+        return episode;
+    });
 
-  // Set up locals after data is initialized
-  app.locals.episodes = episodes;
-  app.locals.projectInfo = project.info;
-  app.locals.projectInfo.currentYear = new Date().getFullYear();
-  
-  // Make cache buster available to templates
-  app.locals.assetUrl = (assetPath) => cacheBuster.getAssetUrl(assetPath);
+    // Set up locals after data is initialized
+    app.locals.episodes = episodes;
+    app.locals.projectInfo = project.info;
+    app.locals.projectInfo.currentYear = new Date().getFullYear();
 
-  console.log('Application initialization complete.');
+    // Make cache buster available to templates
+    app.locals.assetUrl = (assetPath) => cacheBuster.getAssetUrl(assetPath);
+
+    console.log('Application initialization complete.');
 }
 
 // Call the initialization function
@@ -73,22 +99,22 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 // Custom 404 handler
-app.use(function (req, res, next) {
-  res.status(404).render('404', {
-    message: "Запрашиваемая страница не найдена",
-    title: "404 - Страница не найдена",
-    path: req.path || ''
-  });
+app.use(function(req, res, next) {
+    res.status(404).render('404', {
+        message: "Запрашиваемая страница не найдена",
+        title: "404 - Страница не найдена",
+        path: req.path || ''
+    });
 });
 
 // Error handler
-app.use(function (err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-  res.status(err.status || 500);
-  res.render("error", {
-    path: req.path || ''
-  });
+app.use(function(err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+    res.status(err.status || 500);
+    res.render("error", {
+        path: req.path || ''
+    });
 });
 
 // Remove the duplicate port setting and listening here since it's handled in www-techlife
