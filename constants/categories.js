ca56eb9130b4ad7f821881c9869d6f26a442c0bb
Vasily S. Myazin <productizement@gmail.com>
2025-08-27 14:50:54 -0300

    fix: create missing constants/categories.js file
      - Add missing constants directory and categories.js module
      - Include all podcast categories from analysis data

---
+++ constants/categories.js
@@ -0,0 +1,18 @@
+// constants/categories.js
+// Centralized storage for podcast episode categories
+
+const CATEGORY_NAMES = [
+    "Искусственный интеллект",
+    "Устройства и гаджеты", 
+    "Программное обеспечение",
+    "Интернет-технологии",
+    "Конфиденциальность и безопасность",
+    "Технологические компании",
+    "Культура и общество",
+    "Философия",
+    "Путешествия"
+];
+
+module.exports = {
+    CATEGORY_NAMES
+};
